f0877ad07392b17898bb8ac6867e70a1f35f73ba
Pratham Barua Nabo <nabobarua2@gmail.com>
2022-11-04 09:54:04 +0600

    finished

--- js/dashboard.js
+++ js/dashboard.js
@@ -28,3 +28,40 @@ document.getElementById('deposit-btn-tag').addEventListener('click',function(){
     balanceInput.innerText = totalBalance;
 
 })
+
+
+
+document.getElementById('withdraw-btn-tag').addEventListener('click',function(){
+    const withdrawInput = document.getElementById('with-input-tag') ;
+    const withdraw = withdrawInput.value;
+    const withdrawMain = parseFloat(withdraw);
+
+    withdrawInput.value = '';
+
+    if(isNaN(withdrawMain)){
+        alert("Please Enter Any Number Of Amount");
+        return;
+    }
+    
+
+    const balanceInput = document.getElementById('total-balance');
+    const balance = balanceInput.innerText;
+    const balanceMain = parseFloat(balance);
+
+    if(withdraw>balanceMain){
+        alert("You Do Not Have Sufficiant Balance");
+        return;
+    }
+
+    const withdrawText = document.getElementById('total-withdraw') ;
+    const withdrawElement = withdrawText.innerText;
+    const mainWithdraw = parseFloat(withdrawElement);
+
+
+    const totalWithdraw = withdrawMain + mainWithdraw;
+    withdrawText.innerText =totalWithdraw;
+
+
+    const totalBalance = balanceMain - withdrawMain;
+    balanceInput.innerText = totalBalance;
+})
